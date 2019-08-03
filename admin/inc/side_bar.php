f623156cb0bae3650ac10a0b396623a5d35b4e2d
kanak <kanakdebnath826@gmail.com>
2019-08-03 14:34:16 +0600

    sidebar setup.

--- admin/inc/side_bar.php
+++ admin/inc/side_bar.php
@@ -23,4 +23,18 @@
       <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/example" class="nav-link<?php active_page($data, 'example'); ?>">example</a></li>
     </ul>
   </li>
+
+
+
+  <?php $nav_items_section_2 = ['software-status', 'software-language','software-details']; ?>
+  <li class="nav-item nav-item-submenu<?php echo nav_item_open($nav_items_section_2, $data['page_index']) ?>">
+    <a href="#" class="nav-link"><i class="icon-copy"></i> <span>Software Setup</span></a>
+
+    <ul class="nav nav-group-sub" data-submenu-title="Layouts" <?php echo nav_item_open($nav_items_section_2, $data['page_index']) ? 'style="display: block;"' : ''; ?>>
+      <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/software-status" class="nav-link<?php active_page($data, 'software-status'); ?>">Software Status</a></li>
+      <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/software-language" class="nav-link<?php active_page($data, 'software-language'); ?>">Software Language</a></li>
+      <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/software-details" class="nav-link<?php active_page($data, 'software-details'); ?>">Software Details</a></li>
+    </ul>
+  </li>
+
 </ul>
