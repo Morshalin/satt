6eb2db3013af848e25e17786b4d9cdd38d40b700
Your Name <sohag.ice.ru@gmail.com>
2019-08-18 12:29:47 +0600

    setup section form side bar is removed

--- admin/inc/side_bar.php
+++ admin/inc/side_bar.php
@@ -13,16 +13,7 @@
 
 
 
-  <?php $nav_items_section_1 = ['course', 'venue','example']; ?>
-  <li class="nav-item nav-item-submenu<?php echo nav_item_open($nav_items_section_1, $data['page_index']) ?>">
-    <a href="#" class="nav-link"><i class="icon-copy"></i> <span>Setup</span></a>
 
-    <ul class="nav nav-group-sub" data-submenu-title="Layouts" <?php echo nav_item_open($nav_items_section_1, $data['page_index']) ? 'style="display: block;"' : ''; ?>>
-      <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/course" class="nav-link<?php active_page($data, 'course'); ?>">Courses</a></li>
-      <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/venue" class="nav-link<?php active_page($data, 'venue'); ?>">Venues</a></li>
-      <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/example" class="nav-link<?php active_page($data, 'example'); ?>">example</a></li>
-    </ul>
-  </li>
 
 
 
