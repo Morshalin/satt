bf69d489073d385d3e6ca5aaf752448a318f040e
Your Name <sohag.ice.ru@gmail.com>
2019-08-26 11:45:09 +0600

    agent panel started

--- agent-panel/inc/side_bar.php
+++ agent-panel/inc/side_bar.php
@@ -3,7 +3,7 @@
   <!-- Main -->
   <li class="nav-item-header"><div class="text-uppercase font-size-xs line-height-xs">Main</div> <i class="icon-menu" title="Main"></i></li>
   <li class="nav-item">
-    <a href="<?php echo AGETN_URL; ?>" class="nav-link<?php active_page($data, 'dashboard'); ?>">
+    <a href="<?php echo AGENT_URL; ?>" class="nav-link<?php active_page($data, 'dashboard'); ?>">
       <i class="icon-home4"></i>
       <span>
         Dashboard
@@ -14,17 +14,22 @@
 
 
 
-  <?php $nav_items_section_2 = ['software-status', 'software-language','software-details','software-price']; ?>
-  <li class="nav-item nav-item-submenu<?php echo nav_item_open($nav_items_section_2, $data['page_index']) ?>">
-    <a href="#" class="nav-link"><i class="icon-hammer-wrench"></i> <span>Software Setup</span></a>
+  <?php $nav_items_section_1 = ['agents-customer', 'add-agents-customer']; ?>
 
-    <ul class="nav nav-group-sub" data-submenu-title="Layouts" <?php echo nav_item_open($nav_items_section_2, $data['page_index']) ? 'style="display: block;"' : ''; ?>>
-      <li class="nav-item"><a href="<?php echo AGETN_URL; ?>/software-status" class="nav-link<?php active_page($data, 'software-status'); ?>">Software Status</a></li>
-      <li class="nav-item"><a href="<?php echo AGETN_URL; ?>/software-language" class="nav-link<?php active_page($data, 'software-language'); ?>">Software Language</a></li>
-      <li class="nav-item"><a href="<?php echo AGETN_URL; ?>/software-details" class="nav-link<?php active_page($data, 'software-details'); ?>">Software Details</a></li>
-      <li class="nav-item"><a href="<?php echo AGETN_URL; ?>/software-price" class="nav-link<?php active_page($data, 'software-price'); ?>">Software Price</a></li>
+  <li class="nav-item nav-item-submenu<?php echo nav_item_open($nav_items_section_1, $data['page_index']) ?>">
+    <a href="#" class="nav-link"><i class="icon-hammer-wrench"></i> <span>Customers</span></a>
+
+    <ul class="nav nav-group-sub" data-submenu-title="Layouts" <?php echo nav_item_open($nav_items_section_1, $data['page_index']) ? 'style="display: block;"' : ''; ?>>
+
+      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/agents-customer" class="nav-link<?php active_page($data, 'agents-customer'); ?>">Your Customer List</a></li>
+
+      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/add-agents-customer" class="nav-link<?php active_page($data, 'add-agents-customer'); ?>">Add New Customer</a></li>
+
     </ul>
+
+
   </li>
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>/AGENT" class="nav-link<?php active_page($data, 'agent-registration'); ?>">Add New Agent</a></li>
 
 
 
