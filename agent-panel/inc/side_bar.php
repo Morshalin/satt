ab05eab48a6933090986c0dc0de29a42088ec463
Your Name <sohag.ice.ru@gmail.com>
2019-08-26 14:03:22 +0600

    left sidebar of agent panel done

--- agent-panel/inc/side_bar.php
+++ agent-panel/inc/side_bar.php
@@ -12,27 +12,46 @@
   </li>
 
 
+  
+
+      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/agents-customer" class="nav-link<?php active_page($data, 'agents-customer'); ?>"><i class="icon-users4"></i> Your Customer List</a></li>
+
+  <?php $nav_items_section_2 = ['available-products', 'sell-products', 'pending-orders']; ?>
+
+  <li class="nav-item nav-item-submenu<?php echo nav_item_open($nav_items_section_2, $data['page_index']) ?>">
+    <a href="#" class="nav-link"><i class="icon-cart5"></i> <span>Products Order And Confirmation</span></a>
+
+    <ul class="nav nav-group-sub" data-submenu-title="Layouts" <?php echo nav_item_open($nav_items_section_2, $data['page_index']) ? 'style="display: block;"' : ''; ?>>
+
+      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/available-products" class="nav-link<?php active_page($data, 'available-products'); ?>">Available Products</a></li>
+
+      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/sell-products" class="nav-link<?php active_page($data, 'sell-products'); ?>">Sell Products</a></li>
+      
+      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/pending-orders" class="nav-link<?php active_page($data, 'pending-orders'); ?>">Pending Orders</a></li>
+      
+      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/confirmed-orders" class="nav-link<?php active_page($data, 'confirmed-orders'); ?>">Confirmed Orders</a></li>
+    </ul>
+  </li>
 
 
-  <?php $nav_items_section_1 = ['agents-customer', 'add-agents-customer']; ?>
 
-  <li class="nav-item nav-item-submenu<?php echo nav_item_open($nav_items_section_1, $data['page_index']) ?>">
-    <a href="#" class="nav-link"><i class="icon-hammer-wrench"></i> <span>Customers</span></a>
+  <?php $nav_items_section_3 = ['message-with-customer', 'message-with-admin']; ?>
+  <li class="nav-item nav-item-submenu<?php echo nav_item_open($nav_items_section_3, $data['page_index']) ?>">
+    <a href="#" class="nav-link"><i class="icon-envelope"></i> <span>Messaging</span></a>
 
-    <ul class="nav nav-group-sub" data-submenu-title="Layouts" <?php echo nav_item_open($nav_items_section_1, $data['page_index']) ? 'style="display: block;"' : ''; ?>>
+    <ul class="nav nav-group-sub" data-submenu-title="Layouts" <?php echo nav_item_open($nav_items_section_3, $data['page_index']) ? 'style="display: block;"' : ''; ?>>
 
-      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/agents-customer" class="nav-link<?php active_page($data, 'agents-customer'); ?>">Your Customer List</a></li>
+      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/message-with-customer" class="nav-link<?php active_page($data, 'message-with-customer'); ?>">With Customer</a></li>
 
-      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/add-agents-customer" class="nav-link<?php active_page($data, 'add-agents-customer'); ?>">Add New Customer</a></li>
+      <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/message-with-admin" class="nav-link<?php active_page($data, 'message-with-admin'); ?>">With Admin</a></li>
 
     </ul>
-
-
   </li>
-    <li class="nav-item"><a href="<?php echo BASE_URL; ?>/AGENT" class="nav-link<?php active_page($data, 'agent-registration'); ?>">Add New Agent</a></li>
+
+    <li class="nav-item"><a href="<?php echo AGENT_URL; ?>/your-goals" class="nav-link<?php active_page($data, 'your-goals'); ?>"> <i class="icon-link2"></i> Your Goals</a></li>
 
 
-
+    <li class="nav-item"><a href="<?php echo BASE_URL; ?>/AGENT" class="nav-link<?php active_page($data, 'agent-registration'); ?>"><i class="icon-user"></i>Add New Agent</a></li>
 
 
 </ul>
