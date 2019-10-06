65589d056e863400bb2f04318b604fcbe74b1687
abul khair sohag <sohag.ice.ru@gmail.com>
2019-10-06 11:32:21 +0600

    due collection started

---
+++ admin/existing-software-monthly-pay/ajax_confirm_order.php
@@ -0,0 +1,92 @@
+<?php
+require_once '../../config/config.php';
+ajax();
+Session::checkSession('admin', ADMIN_URL . '/pending-order', 'Pending Order');
+if (isset($_GET['pending_order_id'])) {
+	$pending_order_id = $_GET['pending_order_id'];
+	if ($pending_order_id) {
+		$query = "SELECT * FROM satt_order_products WHERE id = '$pending_order_id'";
+		$result = $db->select($query);
+		if (!$result) {
+			http_response_code(500);
+			die(json_encode(['message' => 'Pending Order Not Found']));
+		}
+	}
+}
+
+
+
+
+// order cancel 
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	$pending_order_id = $_GET['pending_order_id'];
+	if ($pending_order_id) {
+		$error = array();
+
+		$product_id = $_POST['product_id'];
+		$querys = "SELECT * FROM software_details WHERE id = '$product_id'";
+		$results = $db->select($querys);
+		if ($results) {
+			$rows = $results->fetch_assoc();
+			$selling_point = $rows['selling_point'];
+		}
+
+		$agent_id = $_POST['agent_id'];
+		$agent_querys = "SELECT * FROM agent_list WHERE id = '$agent_id'";
+		$agent_results = $db->select($agent_querys);
+		if ($agent_results) {
+			$agent_rows = $agent_results->fetch_assoc();
+			$point = $agent_rows['points'];
+			$new_point = $point + $selling_point;
+		}
+
+		$total_price = $fm->validation($_POST['total_price']);
+		$seling_total_price = $fm->validation($_POST['seling_total_price']);
+		$pay_amount = $fm->validation($_POST['pay_amount']);
+		$due_amount = $fm->validation($_POST['due_amount']);
+		$payment_method = $fm->validation($_POST['payment_method']);
+		$mobile_banking_name = $fm->validation($_POST['mobile_banking_name']);
+		$received_phone_number = $fm->validation($_POST['received_phone_number']);
+		$tx_id = $fm->validation($_POST['tx_id']);
+		$check_no = $fm->validation($_POST['check_no']);
+
+		if (!$seling_total_price) {
+			$error['seling_total_price'] = 'Selling Total Price Field required';
+		}elseif (!$pay_amount) {
+			$error['pay_amount'] = 'Pay Field required';
+		}elseif (!$payment_method) {
+			$error['payment_method'] = 'Payment Method Field required';
+		}
+
+		if ($error) {
+			http_response_code(500);
+			die(json_encode(['errors' => $error, 'message' => 'Something Happend Wrong. Please Check Your Form']));
+		} else {
+			$query = "UPDATE satt_order_products SET 
+			status = '1',
+			seling_total_price = '$seling_total_price',
+			confirm_date = now()
+			 WHERE id = '$pending_order_id'";
+			$result = $db->delete($query);
+			if ($result) {
+				$query1 = "INSERT INTO existing_product_pay (product_order_id, payment_method, check_no, mobile_banking_name, received_phone_number, tx_id, pay_amount, pay_date) VALUES ('$pending_order_id','$payment_method','$check_no', '$mobile_banking_name', '$received_phone_number','$tx_id','$pay_amount', now())";
+				$result1 = $db->insert($query1);
+
+				if ($agent_id) {
+					$agentquery = "UPDATE agent_list SET points = '$new_point' WHERE id='$agent_id'";
+					$agentresult = $db->update($agentquery);
+				}
+			}
+			if ($result1 != false) {
+				die(json_encode(['message' => ' Product Order Confirm Successfull']));
+			} else {
+				http_response_code(500);
+				die(json_encode(['errors' => $error, 'message' => 'Something Happend Wrong. Please Check Your Form']));
+			}
+		}
+	}
+	http_response_code(500);
+	die(json_encode(['message' => 'Something Happend Wrong. Please Try Again Later']));
+}
+
---
+++ assets/js/admin/new-software-monthly-pay.js
@@ -0,0 +1,202 @@
+var tariq = '';
+var flag = true;
+var DatatableButtonsHtml5 = function() {
+    var _componentDatatableButtonsHtml5 = function() {
+        if (!$().DataTable) {
+            console.warn('Warning - datatables.min.js is not loaded.');
+            return;
+        }
+        $(document).on('search.dt', function(e, settings) {
+            cardBlock();
+        });
+        $(document).on('change', 'page.dt', function(e, settings) {
+            cardBlock();
+        });
+        $(document).on('change', 'preInit.dt', function(e, settings) {
+            cardBlock();
+        });
+        $(document).on('change', 'order.dt', function(e, settings) {
+            cardBlock()
+        });
+        $(document).on('change', 'preInit.dt', function(e, settings) {
+            cardBlock();
+        });
+        // Setting datatable defaults
+        $.extend($.fn.dataTable.defaults, {
+            autoWidth: false,
+            responsive: true,
+            dom: '<"dt-buttons-full"B><"datatable-header"fl><"datatable-wrap"t><"datatable-footer"ip>',
+            // dom: '<"datatable-header"fBl><"datatable-wrap"t><"datatable-footer"ip>',
+            language: {
+                search: '<span>Filter:</span> _INPUT_',
+                searchPlaceholder: 'Type to filter...',
+                lengthMenu: '<span>Show:</span> _MENU_',
+                loadingRecords: "Getting records...",
+                paginate: {
+                    'first': 'First',
+                    'last': 'Last',
+                    'next': $('html').attr('dir') == 'rtl' ? '&larr;' : '&rarr;',
+                    'previous': $('html').attr('dir') == 'rtl' ? '&rarr;' : '&larr;'
+                },
+                select: {
+                    rows: {
+                        _: "%d rows Selected",
+                        0: "Click a row to select it",
+                        1: "1 row Selected"
+                    }
+                }
+            }
+        });
+        // Basic initialization
+        tariq = $('.content_managment_table').DataTable({
+            fnDrawCallback: function(oSettings) {
+                dataTableReload();
+            },
+            responsive: {
+                details: {
+                    type: 'column',
+                    target: 'tr'
+                }
+            },
+            columnDefs: [{
+                width: "100px",
+                targets: [0, 8]
+            }, {
+                orderable: false,
+                targets: [7,8]
+            }],
+            order: [1, 'asc'],
+            processing: true,
+            serverSide: true,
+            ajax: $('.content_managment_table').data('url'),
+            columns: [
+                // { data: 'checkbox', name: 'checkbox' },
+                {
+                    data: 'DT_RowIndex'
+                }, {
+                    data: 'expected_name_software'
+                }, {
+                    data: 'customer_name'
+                }, {
+                    data: 'customer_phn'
+                }, {
+                    data: 'agent_name'
+                },{
+                    data: 'agent_phn'
+                }, {
+                    data: 'order_date'
+                }, {
+                    data: 'status'
+                },{
+                    data: 'action'
+                }
+            ]
+        });
+    };
+
+    var _componentRemoteModalLoad = function() {
+        $(document).on('click', '#content_managment', function(e) {
+            e.preventDefault();
+            //open modal
+            $('#modal_remote').modal('show');
+            // it will get action url
+            var url = $(this).data('url');
+            // leave it blank before ajax call
+            $('.modal-body').html('');
+            // load ajax loader
+            $('#modal-loader').show();
+            $.ajax({
+                    url: url,
+                    type: 'Get',
+                    dataType: 'html'
+                })
+                .done(function(data) {
+                    $('.modal-body').html(data).fadeIn(); // load response
+                    $('#modal-loader').hide();
+                    _componentInputSwitchery();
+                     _componentSelect2Modal();
+                    _componentDatePicker();
+                    _modalFormValidation();
+
+                })
+                .fail(function(data) {
+                    $('.modal-body').html('<span style="color:red; font-weight: bold;"> Something Went Wrong. Please Try again later.......</span>');
+                    if (data.responseText) {
+                      new PNotify({
+                          title: 'Opps!',
+                          text: $.parseJSON(data.responseText).message,
+                          type: 'error',
+                          addclass: 'alert alert-styled-left',
+                      });
+                    }
+                    $('#modal-loader').hide();
+                });
+        });
+    };
+
+    return {
+        init: function() {
+            _componentDatatableButtonsHtml5();
+            _componentRemoteModalLoad();
+        }
+    }
+}();
+// Initialize module
+// ------------------------------
+document.addEventListener('DOMContentLoaded', function() {
+    DatatableButtonsHtml5.init();
+});
+
+$(document).ready(function(){
+  
+    $(document).on('change','#payment_method', function(){
+        var payment_method = $("#payment_method").val();
+        if (payment_method == 'check') {
+            $("#check_method").show(500);
+            $("#check_no").attr("required",true);
+        }else{
+            $("#check_method").hide(500);
+            $("#check_no").val("");
+            $("#check_no").attr("required",false);
+
+        }
+    });
+
+       $(document).on('change','#payment_method', function(){
+        var payment_method = $("#payment_method").val();
+        if (payment_method == 'mobile') {
+            $("#mobile_method").show(500);
+            $("#mobile_banking_name").attr("required",true);
+            $("#received_phone_number").attr("required",true);
+        }else{
+            $("#mobile_method").hide(500);
+            $("#mobile_banking_name").val("");
+            $("#received_phone_number").val("");
+            $("#tx_id").val("");
+            $("#mobile_banking_name").attr("required",false);
+            $("#received_phone_number").attr("required",false);
+        }
+    })
+
+
+$(document).on('keyup','#pay_amount',function(){
+    var seling_total_price = parseInt($('#seling_total_price').val());
+    var pay_amount = parseInt($('#pay_amount').val());
+    var due_amount = seling_total_price - pay_amount;
+    $('#due_amount').val(due_amount);
+
+    if (pay_amount > seling_total_price) {
+        alert("Pay amount can't gatter then Selling price");
+       $('#pay_amount').val('');
+       $('#due_amount').val('');
+    }
+});
+
+$(document).on('keyup','#seling_total_price',function(){
+    var seling_total_price = parseInt($('#seling_total_price').val());
+    var pay_amount = parseInt($('#pay_amount').val());
+    var due_amount = seling_total_price - pay_amount;
+    $('#due_amount').val(due_amount);
+});
+
+});
