0ec11a6d957ead8dccc46f47fc9d3700717dd8d4
abul khair sohag <sohag.ice.ru@gmail.com>
2019-10-12 10:26:14 +0600

    started for cashbalance

--- admin/inc/side_bar.php
+++ admin/inc/side_bar.php
@@ -160,7 +160,7 @@
 
 
 
-<?php $nav_items_section_25 = ['convince_bill_report','graphics-report','existing-software-report','new-software-report']; ?>
+<?php $nav_items_section_25 = ['convince_bill_report','graphics-report','existing-software-report','new-software-report','cash-balance-report']; ?>
   <li class="nav-item nav-item-submenu<?php echo nav_item_open($nav_items_section_25, $data['page_index']) ?>">
     <a href="#" class="nav-link"><i class="icon-coins"></i> <span>Reports</span></a>
 
@@ -172,6 +172,7 @@
 
       <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/existing-software-report" class="nav-link<?php active_page($data, 'existing-software-report'); ?>">Existing Software</a></li>
       <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/new-software-report" class="nav-link<?php active_page($data, 'new-software-report'); ?>">New Software</a></li>
+      <li class="nav-item"><a href="<?php echo ADMIN_URL; ?>/cash-balance-report" class="nav-link<?php active_page($data, 'cash-balance-report'); ?>">Cash balance</a></li>
     </ul>
   </li>
 
